a8d36719807c3b49c77c2b4cf42a3dbb114bc1b3
andrei.toader1006 <andrei.toader32@gmail.com>
2018-10-05 13:03:38 +0300

    modularization

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var budgetController = function () {
+var budgetController = (function () {
   var x = 2
   var add = function (a) {
     return x + a
   }
   return {
-    publicTest: function (b) {
-      console.log(add(b))
+    test: function (b) {
+      return add(b)
     }
   }
-}
+})()
+
+var uiController = (function () {})()
+
+var controller = (function (budgetCtrl, uiCtrl) {
+  var z = budgetCtrl.test(5)
+
+  return {
+    anotherTest: function () {
+      console.log(z)
+    }
+  }
+})(budgetController, uiController)
